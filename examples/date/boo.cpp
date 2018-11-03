771f2202c8d67033b50eb284c05b7983f477b4f7
Tux1 <webmastertux1@gmail.com>
2018-11-03 09:44:53 +0800

    update package list and example for "date.h"

--- examples/date/boo.cpp
+++ examples/date/boo.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <date/date.h>
+#include <date/tz.h>
 
 int main() {
   using namespace date;
   using namespace std::chrono;
-
-  auto today = floor<days>(system_clock::now());
+  auto now = system_clock::now();
+  auto now_local = make_zoned(current_zone(), now);
+  auto today = floor<days>(now);
   std::cout << today << '\n';
+  std::cout << "now utc = " << now << std::endl;
+  std::cout << "now local = " << now_local << std::endl;
 }
